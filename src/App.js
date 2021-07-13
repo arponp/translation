2132c3662b2a1a6ea6fa2bade26a1805425a76ae
Arpon Purkayastha <arponp30@gmail.com>
2021-07-13 10:35:07 -0700

    add textbox for user

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, { useState } from 'react';
+// import { detectLanguage, translateLanguage } from './api/translate';
+
 export default function App() {
-  return (
-    <div>
-      <h1>Translation App</h1>
-    </div>
-  );
+    const [textToTranslate, setTextToTranslate] = useState('');
+
+    return (
+        <div>
+            <h1>Translate App</h1>
+            <input
+                type="text"
+                placeholder="Enter translated text here"
+                onChange={e => setTextToTranslate(e.target.value)}
+            />
+            <p>{textToTranslate}</p>
+        </div>
+    );
 }
